e8e65d12510be9a58562c5739ed4c35e0c711a2e
Mahmoud Saad <msaaad202020@gmail.com>
2024-08-03 17:13:53 +0300

    Create Framework Strucure

---
+++ ValifyFaceScanML/ValifyFaceScanML/ValifyFaceScanML.h
@@ -0,0 +1,18 @@
+//
+//  ValifyFaceScanML.h
+//  ValifyFaceScanML
+//
+//  Created by Mahmoud Saad on 03/08/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValifyFaceScanML.
+FOUNDATION_EXPORT double ValifyFaceScanMLVersionNumber;
+
+//! Project version string for ValifyFaceScanML.
+FOUNDATION_EXPORT const unsigned char ValifyFaceScanMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValifyFaceScanML/PublicHeader.h>
+
+
